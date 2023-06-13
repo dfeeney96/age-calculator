d7d79bfacc5c61a35f3815c17e80f115888e3603
dfeeney96 <daniellefeeney96@gmail.com>
2023-06-13 17:19:51 +0100

    introduce form validation, including if fields are blank or year exceeds current year

--- app.js
+++ app.js
@@ -13,11 +13,23 @@ let monthsOfYear = [
     {month: 12,days: 31}
 ]
 
+const currentDay = new Date().getDate();
+const currentMonth = new Date().getMonth() + 1;
+const currentYear = new Date().getFullYear();
+
+let selectedDay = "";
+let selectedMonth = "";
+let selectedYear = "";
+
 
 const submit = document.querySelector("button");
-const dayText = document.querySelector(".day .digit")
-const monthText = document.querySelector(".month .digit")
-const yearText = document.querySelector(".year .digit")
+const dayText = document.querySelector(".day .digit");
+const monthText = document.querySelector(".month .digit");
+const yearText = document.querySelector(".year .digit");
+
+const dayWarning = document.querySelector(".day .warning-message");
+const monthWarning = document.querySelector(".month .warning-message");
+const yearWarning = document.querySelector(".year .warning-message");
 
 
 
@@ -26,16 +38,58 @@ const yearText = document.querySelector(".year .digit")
 submit.addEventListener("click", function(){
     event.preventDefault();
 
-    const currentDay = new Date().getDate();
-    const currentMonth = new Date().getMonth() + 1;
-    const currentYear = new Date().getFullYear();
+    selectedDay = Number(document.getElementById("day").value);
+    selectedMonth = Number(document.getElementById("month").value);
+    selectedYear = Number(document.getElementById("year").value);
+
+    
+
+    if(selectedDay == "" || selectedMonth == "" || selectedYear == "" || selectedYear > currentYear){
+        if(selectedDay == ""){
+            
+            dayWarning.innerText = "Must be a valid date";
+            dayWarning.setAttribute("id", "active");
+        }
+        if(selectedMonth ==""){
+            
+            monthWarning.innerText = "Must be a valid month";
+            monthWarning.setAttribute("id", "active");
+        }
+        if(selectedYear =="" || selectedYear > currentYear ){
+                        
+            yearWarning.setAttribute("id", "active");
+            if(selectedYear > currentYear){
+                yearWarning.innerText = "Must be in the past";
+            } else {
+                yearWarning.innerText = "Must be a valid year";
+            }
+        } 
+    } else {
+        if(dayWarning.getAttribute("id") =="active"){
+            dayWarning.removeAttribute("id", "active")
+        }
+        if(monthWarning.getAttribute("id") =="active"){
+            monthWarning.removeAttribute("id", "active")
+        }
+        if(yearWarning.getAttribute("id") =="active"){
+            yearWarning.removeAttribute("id", "active")
+        }
+
+
+        let differences = calculateDifference(selectedDay, selectedMonth, selectedYear);
+        let dayDifference = differences[0];
+        let monthDifference = differences[1];
+        let yearDifference = differences[2];
+        displayDifferences(dayDifference, monthDifference, yearDifference);
+    }
+
+})
+
+
+
+function calculateDifference(selectedDay, selectedMonth, selectedYear){
 
     let currentDate = currentDay + (currentMonth * 30.5) + (currentYear * 365);
-    
-    let selectedDay = Number(document.getElementById("day").value);
-    let selectedMonth = Number(document.getElementById("month").value);
-    let selectedYear = Number(document.getElementById("year").value);
-
     let selectedDate = selectedDay + (selectedMonth * 30.5) + (selectedYear * 365);
     
 
@@ -44,11 +98,14 @@ submit.addEventListener("click", function(){
     let yearDifference = Math.floor(dateDifference / 365);
     let monthDifference = Math.floor((dateDifference - (yearDifference * 365)) / 30.5);
     let dayDifference = Math.ceil(dateDifference - (yearDifference * 365) - (monthDifference* 30.5));
-    
 
+    return [dayDifference, monthDifference, yearDifference];
+}
+
+
+function displayDifferences (dayDifference, monthDifference, yearDifference) {
     dayText.innerText = dayDifference;
     monthText.innerText = monthDifference;
     yearText.innerText = yearDifference;
-    
 
-})
+}
